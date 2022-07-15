7a41f80f945a8777026e721413fbefe6f3a9b12a
Hassan Zahid <bsef19a513@pucit.edu.pk>
2022-07-15 19:59:17 +0500

    mapping done

--- Exercise/2D_List.py
+++ Exercise/2D_List.py
@@ -1,8 +1,23 @@
-matrix = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-]
-for row in matrix:
-    for items in row:
-        print(items)
+# 2-D List
+# matrix = [
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9]
+# ]
+# for row in matrix:
+#     for items in row:
+#         print(items)
+
+# Dictionary exercise
+
+phone = input("Phone: ")
+digits_mapping = {
+    "1":"One ",
+    "2":"Two ",
+    "3":"Three ",
+    "4":"Four "
+}
+output = ""
+for digits in phone:
+    output += digits_mapping.get(digits)
+print(output)
