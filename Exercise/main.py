1020a292f26b70c689e83740468abb348be668d9
Hassan Zahid <bsef19a513@pucit.edu.pk>
2022-07-15 15:52:36 +0500

    FizzBuzz function done

--- Exercise/main.py
+++ Exercise/main.py
@@ -4,3 +4,11 @@ def max (x,y):
     if x>=y: return x
     else: return y
 # print(max(5,8))
+
+# 2. fizz_bizz function
+def fizz_buzz(x):
+    if x%3==0 and x%5==0: return "FizzBuzz"
+    elif x%3==0: return "Fizz"
+    elif x%5==0: return "Buzz"
+    else: return x
+# print(fizz_buzz(16))
