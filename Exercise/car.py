5ec26116eba25b47c1589b6b8b40da074de50c63
Hassan Zahid <bsef19a513@pucit.edu.pk>
2022-07-15 17:11:39 +0500

    Car Exercise updated done

--- Exercise/car.py
+++ Exercise/car.py
@@ -1,13 +1,24 @@
+car_start = False
 while True:
     user_input = input(">")
     if user_input == "help":
-        print("start - to start the car")
-        print("stop - to stop the car")
-        print("quit - to exit")
+        print("""
+start - to start the car
+stop - to stop the car
+quit - to exit
+        """)
     elif user_input == "start":
-        print("Car started...Ready to go")
+        if car_start == False:
+            print("Car started...Ready to go")
+            car_start = True
+        elif car_start == True:
+            print("Car is already started")
     elif user_input == "stop":
-        print("Car stopped")
+        if car_start == True:
+            print("Car stopped")
+            car_start = False
+        elif car_start == False:
+            print("Car is already stopped")
     elif user_input == "quit":
         break
     else:
